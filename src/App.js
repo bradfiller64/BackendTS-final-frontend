4dddc63f5617f15bd1babe034a3c64554a128c36
bradfiller64 <fillerbrad64@gmail.com>
2023-01-21 12:10:19 -0800

    Initial app setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
+import { Route, BrowserRouter, Routes, Link } from 'react-router-dom'
+import './styles/App.css'
+
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
 
