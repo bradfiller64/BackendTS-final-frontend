0f761a7fe7cc5574efd2119ef4a3b4de030e02dd
bradfiller64 <fillerbrad64@gmail.com>
2023-01-21 22:25:00 -0800

    setting up the App & homepage

--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ import './styles/App.css'
 
 function App() {
   return (
-    
+    <div>
+      <BrowserRouter>
+        <nav>
+          <Link to="/signup">Sign Up</Link>
+          <span> | </span>
+          <Link to="/signin">Sign In</Link>
+          <span> | </span>
+          <Link to="/posts">Feed</Link>
+          <hr></hr>
+        </nav>
+        {/* <Routes>
+          <Route exact path="/" element={<SignIn />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes> */}
+      </BrowserRouter>
+    </div>
   );
 }
 
