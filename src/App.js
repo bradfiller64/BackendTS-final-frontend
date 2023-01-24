dfcd4dcf67c2797bdfb7e62651aeff0107abd90b
bradfiller64 <fillerbrad64@gmail.com>
2023-01-23 21:25:32 -0800

    building App routes - Started Contexts/Providers - Started SignUp Component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import { Route, BrowserRouter, Routes, Link } from 'react-router-dom'
+import SignUp from './components/SignUp'
+import { PostProvider } from './contexts/PostProvider';
+import { UserProvider } from './contexts/UserProvider';
 import './styles/App.css'
 
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <nav>
-          <Link to="/signup">Sign Up</Link>
-          <span> | </span>
-          <Link to="/signin">Sign In</Link>
-          <span> | </span>
-          <Link to="/posts">Feed</Link>
-          <hr></hr>
-        </nav>
-        {/* <Routes>
-          <Route exact path="/" element={<SignIn />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes> */}
-      </BrowserRouter>
-    </div>
+    <UserProvider>
+      <PostProvider>
+        <div>
+          <BrowserRouter>
+            <nav>
+              <Link to="/signup">Sign Up</Link>
+              <span> | </span>
+              <Link to="/signin">Sign In</Link>
+              <span> | </span>
+              <Link to="/posts">Feed</Link>
+              <hr></hr>
+            </nav>
+            <Routes>
+              {/* <Route exact path="/" element={<SignIn />} />
+          <Route path="/signin" element={<SignIn />} /> */}
+              <Route path="/signup" element={<SignUp />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </PostProvider>
+    </UserProvider>
   );
 }
 
